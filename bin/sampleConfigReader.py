e2e75dcc6440f7e717a148d30628195b49dee564
thikade <thomas.hikade@gmail.com>
2020-12-18 14:30:38 +0100

    cleanup

--- bin/sampleConfigReader.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-import os
-from lib import configReader
-
-configMap = configReader.readConfig("/home/hhuebler/data/2i/git/cvtPmiDta/scripts/whitelist.config")
-
-print str(configMap)
